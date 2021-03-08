1abdb22fe738ed32e8bf5f4a6335bd471c011b30
Stephen Sanderson <stephen.sanderson@uq.edu.au>
2021-03-09 09:24:24 +1000

    Added temp/nhmesh/deform thermostat

---
+++ src/USER-NHMESH/compute_temp_nhmesh_deform.h
@@ -0,0 +1,56 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef COMPUTE_CLASS
+
+ComputeStyle(temp/nhmesh/deform,ComputeTempDeformNHMesh)
+
+#else
+
+#ifndef LMP_COMPUTE_TEMP_DEFORM_NHMESH_H
+#define LMP_COMPUTE_TEMP_DEFORM_NHMESH_H
+
+#include "compute_temp_deform.h"
+
+namespace LAMMPS_NS {
+
+class ComputeTempDeformNHMesh : public ComputeTempDeform {
+ public:
+  ComputeTempDeformNHMesh(class LAMMPS *, int, char **);
+  virtual ~ComputeTempDeformNHMesh();
+  virtual void compute_array();
+  virtual double memory_usage();
+
+ protected:
+  char *idcoupling;
+  class ComputeCouplingNHMesh *coupling;
+  int n_thermostats;
+
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Compute ID for temp/nhmesh/profile does not exist
+
+Self-explanatory.
+
+E: Invalid coupling compute for temp/nhmesh/profile
+
+Coupling compute must be derived from nhmesh/coupling
+
+*/
