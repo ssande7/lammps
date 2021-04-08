21480f11ce9c64008023dc0c5e0fed4047aa1bfe
Stephen Sanderson <stephen.sanderson@uq.edu.au>
2021-04-08 14:08:39 +1000

    Basic implementation of bin detection for bin1d, bin2d and bin3d. Untested

--- src/USER-MISC/compute_heat_flux_va_chunk.cpp
+++ src/USER-MISC/compute_heat_flux_va_chunk.cpp
@@ -39,7 +39,8 @@ enum{ONCE,NFREQ,EVERY};              // used in several files
 
 /* ---------------------------------------------------------------------- */
 
-ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(LAMMPS *lmp, int narg, char **arg) :
+ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(
+    LAMMPS *lmp, int narg, char **arg) :
   Compute(lmp, narg, arg), cvalues(nullptr), cvalues_local(nullptr)
 {
   if (narg < 6) error->all(FLERR,"Illegal compute stress/mop command");
@@ -55,9 +56,10 @@ ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(LAMMPS *lmp, int narg, char **arg
   int ike = modify->find_compute(id_ke);
   int ipe = modify->find_compute(id_pe);
   if (ike < 0 || ipe < 0)
-    error->all(FLERR,"Could not find compute heat/flux/va compute ID");
+    error->all(FLERR,"Could not find compute heat/flux/va/chunk compute ID");
   if (strcmp(modify->compute[ike]->style,"ke/atom") != 0)
-    error->all(FLERR,"Compute heat/flux/va compute ID does not compute ke/atom");
+    error->all(FLERR,
+        "Compute heat/flux/va/chunk compute ID does not compute ke/atom");
   if (modify->compute[ipe]->peatomflag == 0)
     error->all(FLERR,"Compute heat/flux compute ID does not compute pe/atom");
 
@@ -66,7 +68,7 @@ ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(LAMMPS *lmp, int narg, char **arg
   id_chunk = utils::strdup(arg[5]);
   int ichunk = modify->find_compute(id_chunk);
   if (ichunk < 0) error->all(FLERR,
-      "Chunk/atom compute does not exist for compute heat/flux/va");
+      "Chunk/atom compute does not exist for compute heat/flux/va/chunk");
 
   // Velocity bias
 
@@ -78,7 +80,7 @@ ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(LAMMPS *lmp, int narg, char **arg
       id_temp = utils::strdup(arg[7]);
       int itemp = modify->find_compute(id_temp);
       if (itemp < 0)
-        error->all(FLERR,"Could not find compute heat/flux/va compute ID");
+        error->all(FLERR,"Could not find compute heat/flux/va/chunk compute ID");
       c_temp = modify->compute[itemp];
       if (!c_temp->tempbias)
         error->all(FLERR,"Bias compute does not calculate a velocity bias");
@@ -86,18 +88,20 @@ ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(LAMMPS *lmp, int narg, char **arg
 
   c_chunk = dynamic_cast<ComputeChunkAtom *>(modify->compute[ichunk]);
   if (c_chunk == nullptr)
-    error->all(FLERR,"Compute heat/flux/va requires chunk/atom compute");
+    error->all(FLERR,"Compute heat/flux/va/chunk requires chunk/atom compute");
 
   // Need chunks with volume
   if (c_chunk->which == ArgInfo::TYPE || c_chunk->which == ArgInfo::MOLECULE ||
       c_chunk->which == ArgInfo::COMPUTE || c_chunk->which == ArgInfo::FIX ||
       c_chunk->which == ArgInfo::VARIABLE)
-    error->all(FLERR,"Unsupported chunk/atom compute for compute heat/flux/va");
+    error->all(FLERR,
+        "Unsupported chunk/atom compute for compute heat/flux/va/chunk");
 
   // Can't discard empty chunks since they could still have some heat flux due
   // to the configurational component
   if (c_chunk->compress)
-    error->all(FLERR,"Unsupported chunk/atom compute for compute heat/flux/va");
+    error->all(FLERR,
+        "Unsupported chunk/atom compute for compute heat/flux/va/chunk");
 
   // Warn about idsflag == ONCE - invalid if atoms can move into different
   // chunks
@@ -108,7 +112,7 @@ ComputeHeatFluxVAChunk::ComputeHeatFluxVAChunk(LAMMPS *lmp, int narg, char **arg
   // 3D only since calculations are based on chunk volumes
   if (domain->dimension < 3)
     error->all(FLERR,
-        "Compute heat/flux/va incompatible with simulation dimension");
+        "Compute heat/flux/va/chunk incompatible with simulation dimension");
 
   c_ke = modify->compute[ike];
   c_pe = modify->compute[ipe];
@@ -145,8 +149,10 @@ void ComputeHeatFluxVAChunk::allocate()
   size_array_rows = nchunk;
   if (nchunk > maxchunk) {
     maxchunk = nchunk;
-    memory->grow(array,size_array_rows,size_array_cols,"heat/flux/va:array");
-    memory->grow(cvalues_local,size_array_rows,size_array_cols,"heat/flux/va:cvalues_local");
+    memory->grow(array,size_array_rows,
+        size_array_cols,"heat/flux/va/chunk:array");
+    memory->grow(cvalues_local,size_array_rows,
+        size_array_cols,"heat/flux/va/chunk:cvalues_local");
   }
 }
 
@@ -162,35 +168,30 @@ void ComputeHeatFluxVAChunk::init()
 
   // Error check
 
-  // Compute stress/mop requires fixed simulation box
-  // TODO: CHECK IF THIS IS A PROBLEM
-  // if (domain->box_change_size || domain->box_change_shape || domain->deform_flag)
-  //   error->all(FLERR, "Compute stress/mop requires a fixed simulation box");
-
   // This compute requires a pair style with pair_single method implemented
 
   if (force->pair == nullptr)
-    error->all(FLERR,"No pair style is defined for compute heat/flux/va");
+    error->all(FLERR,"No pair style is defined for compute heat/flux/va/chunk");
   if (force->pair->single_enable == 0)
-    error->all(FLERR,"Pair style does not support compute heat/flux/va");
+    error->all(FLERR,"Pair style does not support compute heat/flux/va/chunk");
 
   // Warnings
 
   if (me==0) {
 
-    //Compute stress/mop only accounts for pair interactions.
-    // issue a warning if any intramolecular potential or Kspace is defined.
+    // Only accounts for pair interactions at this stage.
+    // Issue a warning if any intramolecular potential or Kspace is defined.
 
     if (force->bond!=nullptr)
-      error->warning(FLERR,"compute heat/flux/va does not account for bond potentials");
+      error->warning(FLERR,"compute heat/flux/va/chunk does not account for bond potentials");
     if (force->angle!=nullptr)
-      error->warning(FLERR,"compute heat/flux/va does not account for angle potentials");
+      error->warning(FLERR,"compute heat/flux/va/chunk does not account for angle potentials");
     if (force->dihedral!=nullptr)
-      error->warning(FLERR,"compute heat/flux/va does not account for dihedral potentials");
+      error->warning(FLERR,"compute heat/flux/va/chunk does not account for dihedral potentials");
     if (force->improper!=nullptr)
-      error->warning(FLERR,"compute heat/flux/va does not account for improper potentials");
+      error->warning(FLERR,"compute heat/flux/va/chunk does not account for improper potentials");
     if (force->kspace!=nullptr)
-      error->warning(FLERR,"compute heat/flux/va does not account for kspace contributions");
+      error->warning(FLERR,"compute heat/flux/va/chunk does not account for kspace contributions");
   }
 
   // request an occasional half neighbor list
@@ -307,8 +308,14 @@ void ComputeHeatFluxVAChunk::compute_flux()
   double *vi;
   double *vj;
 
-  double cfactor[nchunk];
-  int c_ids[nchunk];
+  // cfactor stores fraction of rij in each segment. Allocate nchunk+2 in case
+  // rij crosses the full region of chunks, with xi and xj outside the region
+  double cfactor[nchunk+2];
+  //
+  // c_ids calculation might store one extra value, so overallocate by 1
+  int c_ids[nchunk+3];
+
+  // Number of rij segments
   int n_intersect;
 
   if (biasflag) {
@@ -376,11 +383,12 @@ void ComputeHeatFluxVAChunk::compute_flux()
       confpair[2] = rij[2] * pairdot;
 
       n_intersect = find_crossing(ichunk[i],ichunk[j],xi,rij,c_ids,cfactor);
-      for (int ci=0, c=c_ids[ci]; c < n_intersect; c++) {
-        cvalues_local[c][0] -= cfactor[ci] * confpair[0];
-        cvalues_local[c][1] -= cfactor[ci] * confpair[1];
-        cvalues_local[c][2] -= cfactor[ci] * confpair[2];
-      }
+      for (int ci=0, c=c_ids[ci]; c < n_intersect; c++)
+        if (c >= 0) {
+          cvalues_local[c][3] -= cfactor[ci] * confpair[0];
+          cvalues_local[c][4] -= cfactor[ci] * confpair[1];
+          cvalues_local[c][5] -= cfactor[ci] * confpair[2];
+        }
 
     }
   }
@@ -397,6 +405,11 @@ void ComputeHeatFluxVAChunk::compute_flux()
       }
     }
 
+  for (m = 0; m < nchunk; m++) {
+    cvalues_local[m][0] += cvalues_local[m][3];
+    cvalues_local[m][1] += cvalues_local[m][4];
+    cvalues_local[m][2] += cvalues_local[m][5];
+  }
 
   if (biasflag) c_temp->restore_bias_all();
 
@@ -423,7 +436,7 @@ int ComputeHeatFluxVAChunk::find_crossing(
     return crossing_bincylinder(ci, cj, xi, rij, c_ids, cfactor);
 
   if (c_chunk->which == ArgInfo::BIN1D)
-    return crossing_bin1d(ci, cj, xi, rij, c_ids, cfactor);
+    return crossing_bin1d(ci, cj, xi, rij, 0, c_ids, cfactor);
 
   if (c_chunk->which == ArgInfo::BIN2D)
     return crossing_bin2d(ci, cj, xi, rij, c_ids, cfactor);
@@ -439,21 +452,20 @@ int ComputeHeatFluxVAChunk::find_crossing(
 
 /*------------------------------------------------------------------------*/
 
-int ComputeHeatFluxVAChunk::crossing_bin1d(
-    int ci, int cj, double *xi, double *rij, int *c_ids, double *cfactor)
+int ComputeHeatFluxVAChunk::crossing_bin1d(int ci, int cj, double *xi,
+    double *rij, int cdim, int *c_ids, double *cfactor)
 {
   if (ci == cj && ci > 0) {
     cfactor[0] = 1.0;
     c_ids[0] = ci-1;
     return 1;
   }
-
   int n_intersect;
-  int dim = c_chunk->dim[0];
+  int dim = c_chunk->dim[cdim];
   int dir = rij[dim] > 0 ? 1 : -1;
-  double delta = c_chunk->delta[0];
-  double offset = c_chunk->offset[0];
-  double invdelta = c_chunk->invdelta[0];
+  double delta = c_chunk->delta[cdim];
+  double offset = c_chunk->offset[cdim];
+  double invdelta = c_chunk->invdelta[cdim];
   double ndelta = rij[dim]/delta;
   double x;
   double *boxlo,*boxhi,*prd;
@@ -470,6 +482,10 @@ int ComputeHeatFluxVAChunk::crossing_bin1d(
     }
   }
 
+  c_ids[n_intersect++] = ci-1;
+  double xlast = xi[dim];
+  bool newflag;
+  double rijinv = 1/rij[dim];
   for (int i = 0; i <= ndelta; i++) {
     x = xi[dim] + i*delta*dir;
     if (periodicity) {
@@ -479,24 +495,174 @@ int ComputeHeatFluxVAChunk::crossing_bin1d(
     int ibin = static_cast<int>((x - offset)*invdelta);
     if (x < offset) ibin--;
 
-    if (i == 0 && ibin >= 0) {
-      cfactor[n_intersect] = fabs(offset+delta*ibin - x);
-      if (dir < 0) cfactor[n_intersect] = delta - cfactor[n_intersect];
-      cfactor[n_intersect] /= rij[dim];
-      c_ids[n_intersect] = ibin;
-      n_intersect++;
-    } else if (i > ndelta-1 && ibin >= 0) {
-      cfactor[n_intersect] = fabs(offset+delta*ibin - x);
-      if (dir > 0) cfactor[n_intersect] = delta - cfactor[n_intersect];
-      cfactor[n_intersect] /= rij[dim];
-      c_ids[n_intersect] = ibin;
-      n_intersect++;
-    } else if (ibin >= 0) {
-      cfactor[n_intersect] = fabs(delta/rij[dim]);
-      c_ids[n_intersect] = ibin;
+    // Check for new intersection or final segment
+    newflag = false;
+    if (i > ndelta-1) {
+      // Make sure it doesn't go beyond the end
+      cfactor[n_intersect-1] = fabs(xi[dim]+rij[dim] - xlast);
+      newflag = true;
+    } else if (ibin != c_ids[n_intersect-1]) {
+      cfactor[n_intersect-1] = fabs(offset+delta*(ibin-(dir-1)/2) - xlast);
+      if (cfactor[n_intersect-1] > prd[dim]/2)
+        cfactor[n_intersect-1] = fabs(cfactor[n_intersect-1] - prd[dim]);
+      newflag = true;
+    }
+    if (newflag) {
+      cfactor[n_intersect] *= dir;
+      xlast += cfactor[n_intersect];
+      cfactor[n_intersect] *= rijinv;
+      if (ibin < c_chunk->nlayers[cdim])
+        c_ids[n_intersect] = ibin;
+      else
+        c_ids[n_intersect] = -1;
       n_intersect++;
     }
   }
 
+  return n_intersect-1;
+}
+
+/*------------------------------------------------------------------------*/
+
+int ComputeHeatFluxVAChunk::crossing_bin2d(
+    int ci, int cj, double *xi, double *rij, int *c_ids, double *cfactor)
+{
+  if (ci == cj && ci > 0) {
+    cfactor[0] = 1.0;
+    c_ids[0] = ci-1;
+    return 1;
+  }
+  int *nlayers = nlayers;
+  int c_ids1[nlayers[0]];
+  double cfactor1[nlayers[0]];
+  int dim1 = c_chunk->dim[0];
+  int ci1, cj1, ci2, cj2;
+  ci1 = (ci-1)/nlayers[1];
+  ci2 = (ci-1) - ci1*nlayers[1];
+  cj1 = (cj-1)/nlayers[1];
+  ci1++; ci2++; cj1++;
+  int n_intersect1 = crossing_bin1d(ci1,cj1,xi,rij,0,c_ids1,cfactor1);
+
+  if (n_intersect1 == 0) return 0;
+
+  int n_intersect = 0;
+  int dim2 = c_chunk->dim[1];
+  int periodicity = domain->periodicity[dim2];
+  double boxlo = domain->boxlo[dim2];
+  double boxhi = domain->boxhi[dim2];
+  double prd = domain->prd[dim2];
+  double xbin;
+  double invdelta = 1/c_chunk->delta[dim2];
+  double xi2[3], rij2[3];
+  xi2[0] = xi[0]; xi2[1] = xi[1]; xi2[2] = xi[2];
+  rij2[0] = rij2[1] = rij2[2] = 0;
+  for (int i1 = 0; i1 < n_intersect1; i1++) {
+    xi2[dim2] += rij2[dim2];
+    rij2[dim2] = rij[dim2]*cfactor[i1];
+    xbin = xi2[dim2]+rij2[dim2];
+    if (periodicity) {
+      if (xbin < boxlo) xbin += prd;
+      if (xbin >= boxhi) xbin -= prd;
+    }
+    cj2 = static_cast<int>((xbin - c_chunk->offset[dim2])*invdelta);
+    if (xbin < c_chunk->offset[dim2]) cj2--;
+    cj2++;
+
+    int n_new = crossing_bin1d(ci2, cj2, xi2, rij2, 1,
+        &c_ids[n_intersect], &cfactor[n_intersect]);
+    for (int n = n_intersect; n < n_intersect+n_new; n++) {
+      cfactor[n] *= cfactor1[i1];
+      if (c_ids[n] < 0 || c_ids1[i1] < 0)
+        c_ids[n] = -1;
+      else
+        c_ids[n] += c_ids1[i1]*nlayers[1];
+    }
+    n_intersect += n_new;
+    ci2 = cj2;
+  }
+
   return n_intersect;
+}
+
+/*------------------------------------------------------------------------*/
+
+int ComputeHeatFluxVAChunk::crossing_bin3d(
+    int ci, int cj, double *xi, double *rij, int *c_ids, double *cfactor)
+{
+  if (ci == cj && ci > 0) {
+    cfactor[0] = 1.0;
+    c_ids[0] = ci-1;
+    return 1;
+  }
+  int *nlayers = c_chunk->nlayers;
+  int c_ids12[nlayers[0]*nlayers[1]];
+  double cfactor12[nlayers[0]];
+  int dim1 = c_chunk->dim[0];
+  int ci12, cj12, ci3, cj3;
+  ci12 = (ci-1)/nlayers[2];
+  ci3 = (ci-1) - ci12*nlayers[2];
+  cj12 = (cj-1)/nlayers[2];
+  ci12++; ci3++; cj12++;
+  int n_intersect12 = crossing_bin2d(ci12,cj12,xi,rij,c_ids12,cfactor12);
+
+  if (n_intersect12 == 0) return 0;
+
+  int n_intersect = 0;
+  int dim3 = c_chunk->dim[2];
+  int periodicity = domain->periodicity[dim3];
+  double boxlo = domain->boxlo[dim3];
+  double boxhi = domain->boxhi[dim3];
+  double prd = domain->prd[dim3];
+  double invdelta = 1/c_chunk->delta[dim3];
+  double xbin;
+  double xi3[3], rij3[3];
+  xi3[0] = xi[0]; xi3[1] = xi[1]; xi3[2] = xi[2];
+  rij3[0] = rij3[1] = rij3[2] = 0;
+  for (int i12 = 0; i12 < n_intersect12; i12++) {
+    xi3[dim3] += rij3[dim3];
+    rij3[dim3] = rij[dim3]*cfactor[i12];
+    xbin = xi3[dim3]+rij3[dim3];
+    if (periodicity) {
+      if (xbin < boxlo) xbin += prd;
+      if (xbin >= boxhi) xbin -= prd;
+    }
+    cj3 = static_cast<int>(
+        (xbin - c_chunk->offset[dim3])*invdelta);
+    if (xbin < c_chunk->offset[dim3]) cj3--;
+    cj3++;
+
+    int n_new = crossing_bin1d(ci3, cj3, xi3, rij3, 1,
+        &c_ids[n_intersect], &cfactor[n_intersect]);
+    for (int n = n_intersect; n < n_intersect+n_new; n++) {
+      cfactor[n] *= cfactor12[i12];
+      if (c_ids[n] < 0 || c_ids12[i12] < 0)
+        c_ids[n] = -1;
+      else
+        c_ids[n] += c_ids12[i12]*nlayers[2];
+    }
+    n_intersect += n_new;
+    ci3 = cj3;
+  }
+
+  return n_intersect;
+}
+
+/*------------------------------------------------------------------------*/
+
+int ComputeHeatFluxVAChunk::crossing_binsphere(
+    int ci, int cj, double *xi, double *rij, int *c_ids, double *cfactor)
+{
+  error->all(FLERR, "Spherical binning is currently unimplemented in "
+      "compute heat/flux/va/chunk");
+  return 0;
+}
+
+/*------------------------------------------------------------------------*/
+
+int ComputeHeatFluxVAChunk::crossing_bincylinder(
+    int ci, int cj, double *xi, double *rij, int *c_ids, double *cfactor)
+{
+  error->all(FLERR, "Cylinder binning is currently unimplemented in "
+      "compute heat/flux/va/chunk");
+  return 0;
 }
--- src/USER-MISC/compute_heat_flux_va_chunk.h
+++ src/USER-MISC/compute_heat_flux_va_chunk.h
@@ -41,7 +41,7 @@ private:
 
   void compute_flux();
   int find_crossing(int, int, double *, double *, int *, double *);
-  int crossing_bin1d(int, int, double *, double *, int *, double *);
+  int crossing_bin1d(int, int, double *, double *, int, int *, double *);
   int crossing_bin2d(int, int, double *, double *, int *, double *);
   int crossing_bin3d(int, int, double *, double *, int *, double *);
   int crossing_binsphere(int, int, double *, double *, int *, double *);
@@ -79,19 +79,19 @@ Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
 
-E: Could not find compute heat/flux/va compute ID
+E: Could not find compute heat/flux/va/chunk compute ID
 
 Self-explanatory.
 
-E: Compute heat/flux/va compute ID does not compute ke/atom
+E: Compute heat/flux/va/chunk compute ID does not compute ke/atom
 
 Self-explanatory.
 
-E: Compute heat/flux/va compute ID does not compute pe/atom
+E: Compute heat/flux/va/chunk compute ID does not compute pe/atom
 
 Self-explanatory.
 
-E: Compute heat/flux/va requires chunk/atom compute
+E: Compute heat/flux/va/chunk requires chunk/atom compute
 
 Self-explanatory.
 
@@ -99,26 +99,27 @@ E: Bias compute does not calculate a velocity bias
 
 Self-explanatory.
 
-E: No pair style is defined for compute heat/flux/va
+E: No pair style is defined for compute heat/flux/va/chunk
 
-Self-explanatory. Compute heat/flux/va requires the definition of a pair style.
+Self-explanatory. Compute heat/flux/va/chunk requires the
+definition of a pair style.
 
-E: Pair style does not support compute heat/flux/va
+E: Pair style does not support compute heat/flux/va/chunk
 
 The pair style does not have a single() function, so it can
-not be invoked by compute heat/flux/va.
+not be invoked by compute heat/flux/va/chunk.
 
-W: compute heat/flux/va does not account for bond potentials
+W: compute heat/flux/va/chunk does not account for bond potentials
 
-W: compute heat/flux/va does not account for angle potentials
+W: compute heat/flux/va/chunk does not account for angle potentials
 
-W: compute heat/flux/va does not account for dihedral potentials
+W: compute heat/flux/va/chunk does not account for dihedral potentials
 
-W: compute heat/flux/va does not account for improper potentials
+W: compute heat/flux/va/chunk does not account for improper potentials
 
-W: compute heat/flux/va does not account for kspace contributions
+W: compute heat/flux/va/chunk does not account for kspace contributions
 
-Compute heat/flux/va only accounts for pairwise additive interactions for
+Compute heat/flux/va/chunk only accounts for pairwise additive interactions for
 the computation of local stress tensor components.
 
 */
